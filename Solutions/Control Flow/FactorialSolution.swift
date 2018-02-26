ab0aa60bec1427a48e8749f45144c02bcfcc48ea
Craig Hassig <craig@goodmeetingapp.com>
2018-02-26 10:30:06 -0600

    Factorial Koan complete

---
+++ Solutions/Control Flow/FactorialSolution.swift
@@ -0,0 +1,65 @@
+//------------------------------------------------------------------------------
+//  FactorialSolution.swift
+//  SwiftKoans
+//
+//  Created by Craig Hassig on 2/26/18.
+//  Copyright (c) 2018 Craig Hassig, released under MIT License
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//	Definition:
+//
+//	The factorial of a non-negative integer n, denoted n!, is the product of all
+//	positive integers less than or equal to n.
+//	For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
+//	By definition, 0! = 1
+//------------------------------------------------------------------------------
+
+class FactorialSolution {
+	//---------------------------------------------------------------------------
+	//	Solution:
+	//		Use recursion
+	//---------------------------------------------------------------------------
+	func factorial(_ seed: UInt) -> UInt64 {
+		var result: UInt64 = 1	// covers 1! and 0! cases by default
+		
+		if seed > 1 {
+			result = UInt64(seed) * factorial(seed - 1)
+		}
+		
+		return result
+	}
+	
+	//---------------------------------------------------------------------------
+	//	Solution:
+	//		Use a while loop
+	//---------------------------------------------------------------------------
+	func factorial_1(_ seed: UInt) -> UInt64 {
+		var result: UInt64 = 1	// covers 1! and 0! cases by default
+		
+		var iter: UInt64 = 2
+		while iter <= seed {
+			result *= iter
+			iter += 1
+		}
+		
+		return result
+	}
+	
+	//---------------------------------------------------------------------------
+	//	Solution:
+	//		Use a for-in loop
+	//---------------------------------------------------------------------------
+	func factorial_3(_ seed: UInt) -> UInt64 {
+		var result: UInt64 = 1	// covers 1! and 0! cases by default
+		
+		if seed >= 2 {
+			for iter in 2...seed {
+				result *= UInt64(iter)
+			}
+		}
+		
+		return result
+	}
+}
+
--- SwiftKoansTests/Control Flow/FactorialTests.swift
+++ SwiftKoansTests/Control Flow/FactorialTests.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Craig Hassig on 2/25/18.
 //  Copyright (c) 2018 Craig Hassig, released under MIT License
-//
 //------------------------------------------------------------------------------
 
 import XCTest
@@ -13,30 +12,13 @@ import XCTest
 class FactorialTests: XCTestCase {
 	let sut = Factorial()
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testFactorialResults() {
+    func testFactorial() {
 		XCTAssertEqual(sut.factorial(0), 1)
 		XCTAssertEqual(sut.factorial(1), 1)
  		XCTAssertEqual(sut.factorial(2), 2)
 		XCTAssertEqual(sut.factorial(3), 6)
 		XCTAssertEqual(sut.factorial(7), 5040)
 		XCTAssertEqual(sut.factorial(19), 121645100408832000)
-}
-
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+	}
 
 }
