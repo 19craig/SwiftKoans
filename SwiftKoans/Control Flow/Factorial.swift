f29c6c9f45d94aa8ed7ccfe23e82a0831979536d
Craig Hassig <craig@goodmeetingapp.com>
2018-02-25 18:44:56 -0600

    Initial project and first koan

---
+++ SwiftKoans/Control Flow/Factorial.swift
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+//  Factorial.swift
+//  SwiftKoans
+//
+//  Created by Craig Hassig on 2/23/18.
+//  Copyright (c) 2018 Craig Hassig, released under MIT License
+//
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//	Definition:
+//
+//	The factorial of a non-negative integer n is the product of all positive
+//	integers less than or equal to n.
+//	For example, factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+//	By definition, 0! = 1
+//------------------------------------------------------------------------------
+
+class Factorial {
+	//---------------------------------------------------------------------------
+	//	Task:
+	//
+	//	Write the factorial function
+	//	Consider iterative as well as recursive solutions
+	//---------------------------------------------------------------------------
+	func factorial(_ seed: Int) -> UInt64 {
+
+		return 0
+	}
+}
---
+++ SwiftKoans/main.swift
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+//  main.swift
+//  SwiftKoans
+//
+//  Created by Craig Hassig on 2/23/18.
+//  Copyright © 2018 Craig Hassig. All rights reserved.
+//
+//------------------------------------------------------------------------------
+
+import Foundation
+
+print("Hello, World!")
+
